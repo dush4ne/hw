3add960b18e9f4a2a3fce832682fbb9fe34ef6ec
dush4ne <adushane@icloud.com>
2025-02-12 23:39:22 -0800

    test

---
+++ LabForFutureValue.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Aiden Henderson
+# Palomar College CSIT 175
+# Lab 3, Exercise 2
+
+# Variables and constants
+monthly_amt = 0.0
+years = 0
+annual_int_rate = 0.0
+monthly_int_rate = 0.0
+fv = 0.0
+months = 0
+month_num = 1
+
+# msg with Python format specifiers
+msg = "Month: {0:3d} FV: ${1:.2f} Interest: ${2:.2f}"
+interest = 0.0
+tot_interest = 0.0
+
+# input - get monthly amt, period, and interest rate
+monthly_amt = float(input("Enter the monthly amount: "))
+annual_int_rate = 
